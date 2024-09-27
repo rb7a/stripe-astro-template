006260dd495416bbd27aa713c5770c6147474502
rb7a <130118715+rb7a@users.noreply.github.com>
2024-09-27 18:51:31 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,7 @@ export const {
   STRIPE_SECRET_KEY: STRIPE_SECRET_KEY,
 } = import.meta.env
 
-export const CURRENCY = 'eur'
+export const CURRENCY = 'usd'
 export const INSTAGRAM_URL = 'https://www.instagram.com'
 export const X_URL = 'https://twitter.com'
 export const FACEBOOK_URL = 'https://www.facebook.com'
