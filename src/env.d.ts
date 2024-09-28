cfa162290a39e9a513f6b9bf0df1d51613835db5
rb7a <130118715+rb7a@users.noreply.github.com>
2024-09-28 16:11:40 +0800

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-  readonly STRIPE_SECRET_KEY: string;
-}
+///interface ImportMetaEnv {
+///  readonly STRIPE_SECRET_KEY: string;
+///}
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+///interface ImportMeta {
+///  readonly env: ImportMetaEnv;
+///}
